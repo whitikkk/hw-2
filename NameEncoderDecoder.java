3ddfddbd46d45e253e889b7df381d12634474657
whitikkk <89784525+whitikkk@users.noreply.github.com>
2021-09-12 12:57:24 +0300

    Create NameEncoderDecoder.java

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,15 @@
+lass NameEncoderDecoder {
+  public String encode(String name) {
+  return "NOTFORYOU" +              name.replace("e","1").replace("u","2").replace("i","3").replace("o","4")
+    .replace("a","5") + "YESNOTFORYOU";
+  }
+  public String decode(String name) {
+  return name.replace("NOTFORYOU", "")
+    .replace("1","e")
+    .replace("2","u")
+    .replace("3","i")
+    .replace("4","o")
+    .replace("5","a")
+    .replace("YES","");
+  }
+}
